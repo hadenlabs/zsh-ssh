b64e41ff72b888c910b81adc907378b9643c35c9
@slovacus <slovacus@gmail.com>
2018-12-25 15:40:45 -0500

    chore: connect peco (#1)

---
+++ zsh-peco-ssh-connect.zsh
@@ -0,0 +1,9 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+#
+# Defines transfer alias and provides easy command line file and folder sharing.
+#
+# Authors:
+#   Luis Mayta <slovacus@gmail.com>
+#
